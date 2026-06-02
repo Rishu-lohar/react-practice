0ab3642f5d69be6abf7e0bdc1c9774adaee45742
Rishu Lohar <rishulohar266@gmail.com>
2026-06-02 23:37:56 +0530

    Adding Product-Search project

---
+++ product-search/src/data/products.js
@@ -0,0 +1,33 @@
+const products = [
+
+  {
+    id: 1,
+    name: "HP Laptop",
+    category: "laptop",
+    price: 50000
+  },
+
+  {
+    id: 2,
+    name: "Dell Laptop",
+    category: "laptop",
+    price: 65000
+  },
+
+  {
+    id: 3,
+    name: "iPhone",
+    category: "mobile",
+    price: 80000
+  },
+
+  {
+    id: 4,
+    name: "Samsung Phone",
+    category: "mobile",
+    price: 45000
+  }
+
+];
+
+export default products;
